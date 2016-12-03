cb725831a505bdb7185b62284b7f527515bdeb6a
Mike Haas <mikehaas763@gmail.com>
2016-12-02 23:36:28 -0500

    Add ability to store and retrieve items from cache

--- src/lib/lru-cache.ts
+++ src/lib/lru-cache.ts
@@ -2,6 +2,18 @@ interface LruCacheOptions {
     maxItems: number;
 }
 
-export class LruCache<TKey, Tvalue> {
+export class LruCache<TKey, TValue> {
+    private cacheItems = new Map<TKey, TValue>();
+
     constructor(private maxItems: number = 100) { }
+
+    public add(key: TKey, value: TValue) {
+        this.cacheItems.set(key, value);
+
+        return this;
+    }
+
+    public get(key: TKey) {
+        return this.cacheItems.get(key) || null;
+    }
 }
--- src/lib/test.ts
+++ src/lib/test.ts
@@ -7,3 +7,21 @@ test('LruCache Container', (t) => {
     let cache = new LruCache<string, string>();
     t.true(cache instanceof LruCache);
 })
+
+test('LruCache retrieving items added to cache', (t) => {
+    t.plan(3);
+
+    let cache = new LruCache<string, string>();
+    let key1 = 'key1';
+    let value1 = 'value1';
+    let key2 = 'key2';
+    let value2 = 'value2';
+
+    cache
+        .add(key1, value1)
+        .add(key2, value2);
+
+    t.equal(value1, cache.get(key1));
+    t.equal(value2, cache.get(key2));
+    t.notEqual(value1, cache.get(key2));
+})
